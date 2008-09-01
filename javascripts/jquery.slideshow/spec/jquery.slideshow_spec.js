27b49dfd814e63fa2b50aad8dec9e02f7bcf18ab
T.J. VanSlyke <tj@elctech.com>
2008-09-01 14:00:01 +0200

    added swp files to gitignore, set up screw-unit test harness for the slideshow script

---
+++ javascripts/jquery.slideshow/spec/jquery.slideshow_spec.js
@@ -0,0 +1,19 @@
+Screw.Unit(function() {
+  describe('jquery.slideshow', function() {
+    var slideshow;
+
+    before(function() {
+      var element = $('<div></div>');
+      slideshow = element.slideshow();
+    });
+
+    describe('incrementing the slide', function() {
+
+      it('increments the slide index', function() {
+      });
+
+    });
+
+  });
+
+});
