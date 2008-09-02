1d70931663e418898471e709fb59f98399eca2a1
T.J. VanSlyke <tj@elctech.com>
2008-09-02 14:03:16 +0200

    wrote first spec, modified specs in slideshow

--- javascripts/jquery.slideshow/lib/jquery.slideshow.js
+++ javascripts/jquery.slideshow/lib/jquery.slideshow.js
@@ -5,66 +5,68 @@ $.fn.slideshow = function(options) {
   const KEY_LEFT  = 37;
   const KEY_RIGHT = 39;
 
-  var opts = $.extend({}, $.fn.slideshow.defaults, options);
+  ////////////////////////////////////////
+  var presentationMethods = {
+    _slideIndex:  0,
+    slideIndex: function() { return this._slideIndex; },
+    slides: [],
+    init: function() {
+      this.addClass('presentation');
+      var slideshow = this;
+      slideshow.children('.'+opts.slideClass).each(function() {
+        // Hide all but the first slide.
+        if (slideshow.slides.length > 0) {
+          $(this).hide();
+        }
+        // Push each slide onto the queue.
+        slideshow.slides.push($(this));
+      });
+    },
 
+    current: function() { return this.slides[this.slideIndex()]; },
+
+    nextSlide: function() {
+      if (this.slideIndex() < this.slides.length-1) {
+        this.current().hide();
+        this._slideIndex++;
+        this.current().show();
+      }
+    },
+    previousSlide: function() {
+      if (this.slideIndex() > 0) {
+        this.current().hide();
+        this._slideIndex--;
+        this.current().show();
+      }
+    }
+  };
+
+  //////////////////////////////////////// 
+
+  var opts = $.extend({}, $.fn.slideshow.defaults, options);
   var slideshow         = $(this);
 
-  return this.each(function() {
+  if (opts.format == 'presentation') {
+    $.extend(slideshow, presentationMethods);
+    slideshow.init();
+    $(window).keydown(function(e) {
+      if      (e.keyCode == KEY_LEFT) {
+        slideshow.previousSlide();
+      }
+      else if (e.keyCode == KEY_RIGHT) {
+        slideshow.nextSlide();
+      }
+    });
+  }
+  else if (opts.format == 'outline') {
+    slideshow.addClass('outline');
+  }
 
-    if (opts.format == 'presentation') {
-
-      $.extend(slideshow, {
-        index:  0,
-        slides: [],
-        init: function() {
-          this.addClass('presentation');
-          var slideshow = this;
-          slideshow.children('.'+opts.slideClass).each(function() {
-            // Hide all but the first slide.
-            if (slideshow.slides.length > 0) {
-              $(this).hide();
-            }
-            // Push each slide onto the queue.
-            slideshow.slides.push($(this));
-          });
-        },
-        next: function() {
-          if (this.index < this.slides.length-1) {
-            this.slides[this.index].hide();
-            this.index++;
-            this.slides[this.index].show();
-          }
-        },
-        previous: function() {
-          if (this.index > 0) {
-            this.slides[this.index].hide();
-            this.index--;
-            this.slides[this.index].show();
-          }
-        }
-      });
-
-      slideshow.init();
-      $(window).keydown(function(e) {
-        if      (e.keyCode == KEY_LEFT) {
-          slideshow.previous();
-        }
-        else if (e.keyCode == KEY_RIGHT) {
-          slideshow.next();
-        }
-      });
-
-    }
-
-    else if (opts.format == 'outline') {
-
-      slideshow.addClass('outline');
-
-    }
-
-  });
+  return slideshow;
 
 };
+
+//////////////////////////////////////// 
 
 $.fn.slideshow.defaults = {
   slideClass: 'slide',
--- javascripts/jquery.slideshow/spec/jquery.slideshow_spec.js
+++ javascripts/jquery.slideshow/spec/jquery.slideshow_spec.js
@@ -1,15 +1,38 @@
 Screw.Unit(function() {
   describe('jquery.slideshow', function() {
     var slideshow;
+    var slides;
 
     before(function() {
-      var element = $('<div></div>');
-      slideshow = element.slideshow();
+      slideshow = $('<div class="slideshow">test</div>').slideshow();
+      slides = 
+        [
+          $('<div class="slide"></div>'),
+          $('<div class="slide"></div>'),
+          $('<div class="slide"></div>')
+        ];
+      slideshow.slides = slides;
     });
 
-    describe('incrementing the slide', function() {
+    describe('slideIndex()', function() {
+      it('defaults to zero', function() {
+        expect(slideshow.slideIndex()).to(equal, 0);
+      });
+    });
 
-      it('increments the slide index when the right-arrow key is pressed', function() {
+    describe('nextSlide()', function() {
+
+      it('hides the current slide', function() {
+        //expect(slides[0]).to(receive, 'hide');
+      });
+
+      it('increments the slide index', function() {
+        expect(slideshow.slideIndex()).to(equal, 0);
+        slideshow.nextSlide();
+        expect(slideshow.slideIndex()).to(equal, 1);
+      });
+
+      it('shows the current slide', function() {
       });
 
     });
